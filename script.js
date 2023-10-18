bc0cd49e5f0c026b3d2f207cdb5a0a120780f849
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-10-18 16:05:22 +0100

    main task done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Write your code here
+count = 0;
+
+function bulb1() {
+  count++;
+  document.querySelector("#counter-text").innerHTML = count;
+  document.querySelector("#lightbulb1").classList.toggle("active");
+}
+
+function bulb2() {
+  count++;
+  document.querySelector("#counter-text").innerHTML = count;
+  document.querySelector("#lightbulb2").classList.toggle("active");
+}
+
+function bulb3() {
+  count++;
+  document.querySelector("#counter-text").innerHTML = count;
+  document.querySelector("#lightbulb3").classList.toggle("active");
+}
